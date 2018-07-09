71cb2542a4348962e732b0083a62740ed3c4b326
JShepherd <JShepherd@PC-F46D047BC508.emscrm.com>
2018-07-09 12:51:12 -0500

    Improved description of what the program does.

--- InteractionsCalculator/src/interactionscalculator/InteractionsCalculatorDriver.java
+++ InteractionsCalculator/src/interactionscalculator/InteractionsCalculatorDriver.java
@@ -2,8 +2,11 @@ package interactionscalculator;
 
 /**
  * Made primarily for the Sergeants monthly report, this program reads a CSV
- * file with no header and three items per line: date, time, interactions.
+ * file, filtering for lines that begin with a digit and are length of 3.
+ * Each line contains date, time, and number of interactions in that order.
  * It sums the interactions by hour by day and writes a CSV file with a header.
+ * Each column is a day of week starting at Monday. Each row is an hour starting
+ * at 000.
  * @author JShepherd
  */
 public class InteractionsCalculatorDriver {
