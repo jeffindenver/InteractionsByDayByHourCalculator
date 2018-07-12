f1f9182472860185450dce14b47306f9029032da
JShepherd <JShepherd@emscrm.com>
2018-07-12 07:44:33 -0500

    Added drag and drop listener.

--- InteractionsCalculator/src/interactionscalculator/InteractionsCalculatorController.java
+++ InteractionsCalculator/src/interactionscalculator/InteractionsCalculatorController.java
@@ -1,6 +1,12 @@
 package interactionscalculator;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDropEvent;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  *
@@ -9,35 +15,63 @@ import java.io.IOException;
 public class InteractionsCalculatorController {
 
     private final InteractionsCalculator calculator;
-    private final InteractionsCalculatorView view;
+//    private final InteractionsCalculatorView view;
+    private final CalculatorGUI view;
 
     public InteractionsCalculatorController(InteractionsCalculator calculator,
-            InteractionsCalculatorView view) {
-        
+            CalculatorGUI view) {
+
         this.calculator = calculator;
         this.view = view;
+
     }
 
     public void start() {
-        String prompt = "Enter the filename.";
-        String filename = view.getUserInput(prompt);
-        view.printMessage("Filename set to " + filename);
-        
-        try {
-            calculator.readFile(filename); 
-        } catch (IOException e) {
-            view.printError(e.getMessage());
-        }
 
-        calculator.composeStats();
-        calculator.calculateInteractions();
-        String totals = calculator.writeTotals();
-        view.printMessage(totals);
+        startListeners();
 
-        try {
-            calculator.writeFile(totals);
-        } catch (IOException e) {
-            view.printError(e.getMessage());
-        }
+    }
+
+    private void startListeners() {
+
+        view.getTextArea().setDropTarget(new DropTarget() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public synchronized void drop(DropTargetDropEvent evt) {
+                try {
+                    evt.acceptDrop(DnDConstants.ACTION_COPY);
+                    @SuppressWarnings("unchecked")
+                    List<File> droppedFiles = (List<File>) evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+
+                    for (File file : droppedFiles) {
+                        view.getTextArea().setText(file.toString());
+                        processFile(file.toString());
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            private void processFile(String filename) {
+
+                try {
+                    calculator.readFile(filename);
+                } catch (IOException e) {
+                    view.printError(e.getMessage());
+                }
+
+                calculator.composeStats();
+                calculator.calculateInteractions();
+                String totals = calculator.writeTotals();
+                view.printMessage(totals);
+
+                try {
+                    calculator.writeFile(totals);
+                } catch (IOException e) {
+                    view.printError(e.getMessage());
+                }
+            }
+        });
     }
 }
